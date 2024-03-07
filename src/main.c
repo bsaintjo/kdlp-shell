53b75407c03d8415aadee97f778c0dfc6c7b51c0
Brandon Saint-John (aarch64) <bsaintjo@ucsc.edu>
2024-03-07 14:50:54 -0800

    up to lvl4ish

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 
 #ifdef __unix__
 #include <linux/limits.h>
@@ -10,17 +11,50 @@
 #define PATH_MAX 4096 
 #endif 
 
+int first_word(char *input, int size) {
+    for (int i = 0; i < size; i++) {
+        if (isspace(*input)) {
+            return i;
+        } else {
+            input++;
+        }
+    }
+    return -1;
+}
+
+int run(char *cmd, int size) {
+    if (strncmp(cmd, "exit", size) == 0) {
+        return 1;
+    } else
+    if (strncmp(cmd, "hello", size) == 0) {
+        return 2;
+    } else {
+        return -1;
+    }
+}
+
 int main(void)
 {
     char input[1024];
 
-    while (!feof(stdin)) {
+    while (1) {
         char cwd[PATH_MAX];
         getcwd(cwd, sizeof(cwd));
         printf("%s$ ", cwd);
         char *res = fgets(input, 1024, stdin);
+        if (res == NULL) {
+            break;
+        }
+
+        // Copy first word in command to cmd
+        int first_word_len = first_word(res, 255);
+        char cmd[256] = {0};
+        strncpy(cmd, res, first_word_len);
+        cmd[first_word_len] = '\0'; 
+
         if (res[0] != '\n') {
-            printf("Unrecognized command\n");
+            printf("Unrecognized command: %s\n", cmd);
+            printf("Cmd %d\n", run(cmd, 256));
         }
     }
 
