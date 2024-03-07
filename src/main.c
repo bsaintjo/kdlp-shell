14443a1f3a0bd7bf854fff62293c0807e6e6dcf5
Brandon Saint-John (aarch64) <bsaintjo@ucsc.edu>
2024-03-06 22:57:22 -0800

    finish up to lvl2

--- src/main.c
+++ src/main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
+#ifdef __unix__
 #include <linux/limits.h>
+#endif
+
+#ifdef __APPLE__
+#define PATH_MAX 4096 
+#endif 
 
 int main(void)
 {
-    char cwd[PATH_MAX];
-    getcwd(cwd, sizeof(cwd));
-    printf("Hello world!\n");
-    printf("cwd: %s\n", cwd);
+    char input[1024];
+
+    while (!feof(stdin)) {
+        char cwd[PATH_MAX];
+        getcwd(cwd, sizeof(cwd));
+        printf("%s$ ", cwd);
+        char *res = fgets(input, 1024, stdin);
+        if (res[0] != '\n') {
+            printf("Unrecognized command\n");
+        }
+    }
 
     return 0;
 }
